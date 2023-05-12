e0ed59e68a83c17414481667ab4ec2e888fbfef5
Wiz <wizbarbosa@gmail.com>
2023-05-12 00:28:01 -0500

    client for api reqzzz

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,7 +52,7 @@ export default function Home() {
           onClick={console.log("button pressed")}
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
+            Client{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -94,13 +94,13 @@ export default function Home() {
         </button>
 
         <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          href="/client"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
+            Client{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
